508e77dc735806a7fadaacf477c22a10521da3df
lamiaa <l.alqahtani@safcsp.org.sa>
2024-04-09 07:34:28 +0300

    week 4 project: fill file have been updated to always draw the screen black

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,40 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//addr = SCREEN address (16384)
+@SCREEN
+D=A
+@addr
+M=D
+
+//i is the number of iterations.
+@i
+M=0
+
+//R0 will hold the last address offset of the screen memory map (the maximum number of i). (8191)
+@R0
+D=M
+@max
+M=D
+
+(LOOP)
+@i
+D=M
+@addr
+A=M+D
+M=-1
+@i
+M=M+1
+@max
+D= M-D // if max-i == 0 go to the end of the program.
+@END
+D; JEQ
+
+@LOOP
+0;JMP
+
+
+(END)
+@END
+0;JMP
+
